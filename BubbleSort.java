136bf7bbab418f592c278eeba53c2b0caac22229
67Mx <max.dominguez@stu.daviess.kyschools.us>
2026-03-06 20:49:27 +0000

    commit

--- BubbleSort.java
+++ BubbleSort.java
@@ -6,6 +6,7 @@ public class BubbleSort
 	public static void main(String[] args)
   {
     	int[] myNums = {12, 15, 0, 44, 13, 1, 2};
+		int[] newNums = {12, 15, 0, 44, 13, 1, 2};
 		int passes;
 		int comparisons;
 		int tempNum;
@@ -14,7 +15,7 @@ public class BubbleSort
 		comparisons=passes-1;
 
 		//Loop once for each pass, where passes is one less than the number of items.
-		for(int i=0; i<passes; i++)
+		for(int i=0; i<passes-1; i++)
 		{
 			//Loop once for each comparison, where comparisons are one less than the number of unsorted.
 			for(int m=0; m<comparisons; m++)
@@ -31,8 +32,44 @@ public class BubbleSort
 			}
 			//The next pass will use one less comparison
 			comparisons--;
+			System.out.println(Arrays.toString(myNums));
 		//End Loop for passes
   		}
-		System.out.println(Arrays.toString(myNums));
+
+		System.out.println(Arrays.toString(sortArray(newNums)));
 	}
+	
+	public static int[] sortArray (int[] unsorted)
+    {
+		int passes;
+		int comparisons;
+		int tempNum;
+		passes=unsorted.length;
+		comparisons=passes-1;
+        int[] sorted;
+        sorted=unsorted;
+
+        System.out.println(Arrays.toString(unsorted));
+        for(int i=0; i<passes-1; i++)
+		{
+			//Loop once for each comparison, where comparisons are one less than the number of unsorted.
+			for(int m=0; m<comparisons-1; m++)
+			{
+				//If they are out of order, swap the values
+				if(unsorted[m]>unsorted[m+1])
+				{
+					tempNum=unsorted[m+1];
+					sorted[m+1]=unsorted[m];
+					sorted[m]=tempNum;
+				}
+
+			//End Loop for comparisons
+			}
+			//The next pass will use one less comparison
+			comparisons--;
+			System.out.println(Arrays.toString(sorted));
+		//End Loop for passes
+  		}
+			return sorted;
+    }
 }
